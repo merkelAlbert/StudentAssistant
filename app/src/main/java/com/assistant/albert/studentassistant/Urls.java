ba27a1dab0b5549d9fc8cdd75ecebaaa783453cd
merkelAlbert <merkel.albert1997@gmail.com>
2018-05-20 23:54:11 +0300

    logos added

--- app/src/main/java/com/assistant/albert/studentassistant/Urls.java
+++ app/src/main/java/com/assistant/albert/studentassistant/Urls.java
@@ -1,7 +1,7 @@
 package com.assistant.albert.studentassistant;
 
 public class Urls {
-    public static String root = "http://192.168.0.104:8888/";
+    public static String root = "http://32.204.51.60/";
     public static String homework = root + "homework/";
     public static String schedule = root + "schedule/";
     public static String addHomework = root + "addHomework/";
